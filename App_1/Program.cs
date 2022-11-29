5593daed4902fd36f9072b93259438c8d1d6723b
Nikita Belov <lolvvto@yandex.ru>
2022-11-29 18:30:22 +0700

    Add App 1

---
+++ App_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = FindSequence(n);
+Console.WriteLine(result);
+
+int FindSequence(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    
+    Console.Write(n + ", ");
+    return FindSequence(n - 1);
+}
+
+
